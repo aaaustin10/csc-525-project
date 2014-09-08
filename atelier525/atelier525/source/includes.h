f8e7875a6c2ec50aaefd346844dbd8d66afcda61
austin <austin@austin.com>
2014-09-08 11:20:03 -0500

    Adds back main

---
+++ atelier525/atelier525/source/includes.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDES_H
+	#include <iostream>
+	#include <GL/glut.h>
+	#include "keycodes.h"
+	#define INCLUDES_H
+#endif
---
+++ atelier525/atelier525/source/keycodes.h
@@ -0,0 +1,8 @@
+#ifndef KEYCODES_H
+	#define KEY_DELETE 127
+	#define KEY_BACKSPACE 8
+	#define KEY_RETURN 13
+	#define KEY_ESCAPE 27
+
+	#define KEYCODES_H
+#endif
